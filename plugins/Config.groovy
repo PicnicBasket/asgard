c42458a60c27c3692a97947aebfcbe4355416db7
Matt Walkenhorst <matt@picnicsoftware.com>
2015-07-21 19:05:20 +1000

    remove credentials

---
+++ plugins/Config.groovy
@@ -0,0 +1,15 @@
+grails {
+        awsAccounts=['691915999124']
+        awsAccountNames=['691915999124':'prod']
+}
+secret {
+        accessId='ACCESS'
+        secretKey='SECRET'
+}
+cloud {
+        accountName='prod'
+        publicResourceAccounts=[]
+}
+plugin {
+    advancedUserDataProvider = 'dustAdvancedUserDataProvider'
+}
